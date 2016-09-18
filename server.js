1fe848325e34aa6c2ab8750666092bf949c83185
VishalVSV <vishalsvetrivel@gmail.com>
2016-09-18 15:00:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ var reqPath;
 var app = express();
 app.use(morgan('combined'));
 
-app.get(reqPath, function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', reqPath));
-});
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+
 app.get('/article-two', function (req, res) {
-  res.send('Article 2 requested and will load here');
+ res.send('Article 2 requested and will load here');
 });
 
 app.get('/article-three', function (req, res) {
