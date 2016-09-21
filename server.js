2c7a122758819a668af05f2ceb951830afb771db
VishalVSV <vishalsvetrivel@gmail.com>
2016-09-21 14:53:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,12 +6,77 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title:'Article One | VishalVSV',
+    heading:'Article One',
+    date: 'Sep 21 2016',
+    content:`
+        <p>
+            Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!
+        </p>
+        
+        <p>
+            Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!
+        </p>
+        
+        <p>
+            Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!
+        </p>
+        
+        <p>
+            Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!Blog Beginings!!
+        </p>
+    `
+};
+
+var createHtml = function(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var html = `
+    <!doctype html>
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name='viewport' content='width=device-width,initial-scale=1'>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div>
+            <div>
+                <a href="/">
+                    Home
+                </a>
+            </div>
+            <hr/>
+            <div>
+                <h3>
+                    ${heading}
+                </h3>
+            </div>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>
+    `;
+    return html;
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createHtml(articleOne));
 });
 
 
