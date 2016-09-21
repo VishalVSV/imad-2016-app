0473b5806797dc3a44c9901d2df4642bfeebf902
VishalVSV <vishalsvetrivel@gmail.com>
2016-09-21 16:31:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,13 +5,11 @@ var img = document.getElementById('madi');
 var marginL = 0;
 function moveLeft () {
     marginL += 1;
-    if (marginL <= distance+50) {
+    if (marginL <= 500) {
       img.style.marginLeft = marginL + "px";   
     }
 }
 img.onclick = function () {
-    var distance = img.style.marginLeft;
-    marginL = distance;
-    var inter = setInterval(moveLeft(),50);
+    var inter = setInterval(moveLeft,50);
     
 };
