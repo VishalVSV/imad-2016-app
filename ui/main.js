6704c00f086e3c91cd5aaf02fa8e96deb48f0740
VishalVSV <vishalsvetrivel@gmail.com>
2016-09-23 18:25:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,6 +12,6 @@ button.onclick = function (){
           }
       }  
     };
+    req.open('GET',"http://vishalvsv.imad.hasura-app.io/counter",true);
+    req.send(null);
 };
-req.open('GET',"http://vishalvsv.imad.hasura-app.io/counter",true);
-req.send(null);
