0a3549b659717083ba673673c97678f752141a4a
VishalVSV <vishalsvetrivel@gmail.com>
2016-10-08 20:22:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -67,12 +67,12 @@ var time = setInterval(function(){var req = new XMLHttpRequest();
               names = JSON.parse(names);
               var list = '';
               for (var x = 0;x<names.length;x++){
-                if (names[x].trim() === null || names[x].trim() === "" || names[x].trim() === " ") {
-                     
-                }
-                else{
+                if (names[x].trim() !== null || names[x].trim() !== "" || names[x].trim() !== " ") {
+                    console.log(names[x]);
                     list += "<p class='chmes'>" + names[x] + '</p>';
+                 
                 }
+                
               }
               
               var chm = document.getElementById('chmes');
