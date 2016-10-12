8b078c71f17caf9bca0bf665a2b0210398dbdb8f
VishalVSV <vishalsvetrivel@gmail.com>
2016-10-12 18:47:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -100,7 +100,7 @@ var time = setInterval(function(){var req = new XMLHttpRequest();
                 
               }
               if (chm.innerHTML!==list){
-                console.log(list);
+                console.log(list+"Updated");
                 chm.innerHTML = list;
               }
           }
