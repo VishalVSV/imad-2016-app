5abe566f31f62170ce036a736ca8f8ebbfaafee1
VishalVSV <vishalsvetrivel@gmail.com>
2016-10-14 17:51:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,9 +32,47 @@ var nameInput = document.getElementById('name');
 var chname=document.getElementById('chname');
 var name = nameInput.value;
 var submit = document.getElementById('submit');
-submit.onclick = sub();
-submit.submit = sub();
-var sub = function () {
+submit.onclick = function () {
+    
+    //button.onclick = function (){
+    var req = new XMLHttpRequest();
+    
+    req.onreadystatechange = function () {
+      if(req.readyState === XMLHttpRequest.DONE) {
+          if(req.status === 200) {
+              var names = req.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var x = 0;x<names.length;x++){
+                  if (names[x] === '' || names[x] === null) {
+                      
+                  }else{
+                  list += '<p class="chmes">' + names[x] + '</p>';
+                  }
+              }
+              var chm = document.getElementById('chmes');
+              chm.innerHTML = '' + list;
+              nameInput.value = "";
+          }
+      }  
+    };
+    name =nameInput.value;
+    var acname =chname.value;
+    if(acname===''){
+        acname='Anonymous';
+    }
+    if (name !=='/clear') {
+        req.open('GET',"http://vishalvsv.imad.hasura-app.io/submit-message?name="+acname+":"+name,true);
+        req.send(null);
+    }
+    else{
+        var chm = document.getElementById('chmes');
+        chm.innerHTML = '';
+    }
+    
+};
+
+submit.submit = function () {
     
     //button.onclick = function (){
     var req = new XMLHttpRequest();
