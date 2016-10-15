146ec597e1016340c939ad2b3df27f44c1ead901
VishalVSV <vishalsvetrivel@gmail.com>
2016-10-15 17:22:02 +0530

    [imad-console] Updates ui/cmdj.js

--- ui/cmdj.js
+++ ui/cmdj.js
@@ -1,4 +1,4 @@
-var d$ = document.getElementById();
-var l$ = console.log();
-var chm = d$('chmes');
-l$('Done');
+var d = document.getElementById();
+var l = console.log();
+var chm = d('chmes');
+l('Done');
