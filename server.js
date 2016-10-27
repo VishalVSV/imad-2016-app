310d40b06f5621272fef79750d03b136c88b738c
VishalVSV <vishalsvetrivel@gmail.com>
2016-10-27 20:44:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var e = require('events');
 var child_process = require('child_process');
 child_process.exec(__dirname+'/temp.bat', function(error, stdout, stderr) {
+    console.log('strat');
     console.log(stdout);
 });
 //var io = require("socket.io");
