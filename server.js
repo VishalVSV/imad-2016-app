7c09bb277427af6dccdd1314f949c2ad80d904cb
VishalVSV <vishalsvetrivel@gmail.com>
2016-10-27 20:47:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var e = require('events');
 var child_process = require('child_process');
-child_process.exec(__dirname+'/temp.bat', function(error, stdout, stderr) {
+child_process.exec(__dirname+'temp.bat', function(error, stdout, stderr) {
     console.log('strat');
     console.log(stdout);
 });
