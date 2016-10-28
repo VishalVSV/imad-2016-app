fff3d8aeb83b4c9aebc33710b661e1a2ef6a1a38
VishalVSV <vishalsvetrivel@gmail.com>
2016-10-28 16:09:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ var morgan = require('morgan');
 var path = require('path');
 var e = require('events');
 var app = express();
+var spawn = require('child_process').spawn,
+ls    = spawn('cmd.exe', ['/c', 'temp.bat']);
+
+ls.stdout.on('data', function (data) {
+console.log('stdout: ' + data);
+});
+
+ls.stderr.on('data', function (data) {
+console.log('stderr: ' + data);
+});
+
+ls.on('exit', function (code) {
+console.log('child process exited with code ' + code);
+});
 
 app.use(morgan('combined'));
 
